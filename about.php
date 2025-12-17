63eeadbd313e87f1f6dd670036b444e1c985462d
thenzaibrahimi <ibrahimithenza08@gmail.com>
2025-12-17 09:18:08 -0800

    Personal project

---
+++ about.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+
+    <!-- swiper css file link -->
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css"/>
+    <!-- font awesome sdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+
+    <!-- custom css file link -->
+     <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+<!-- header secton starts -->
+
+<section class="header">
+
+
+<a href="home.php" class="logo">travel.</a>
+
+<nav class="navbar">
+    <a href="home.php">Home</a>
+    <a href="about.php">About</a>
+    <a href="package.php">Package</a>
+    <a href="book.php">Book</a>
+</nav>
+
+<div id="menu-btn" class="fas fa-bars"></div>
+</section>
+<!-- header section ends -->
+
+
+<div class="heading" style="background:url(images/headng-bg-1.png) no-repeat">
+    <h1>About us</h1>
+</div>
+
+
+
+
+
+
+<!-- footer section starts -->
+
+
+<section class="footer">
+
+<div class = "box-container">
+    <div class="box">
+        <h3>Quick links</h3>
+        <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+        <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+        <a href="package.php"> <i class="fas fa-angle-right"></i> Package</a>
+        <a href="book.php"><i class="fas fa-angle-right"></i> Book</a>
+    </div>
+
+
+    <div class="box">
+        <h3>Extra links</h3>
+       <a href="#"> <i class="fas fa-angle-right"></i> Ask questions</a>
+       <a href="#"> <i class="fas fa-angle-right"></i> About us/a>
+       <a href="#"> <i class="fas fa-angle-right"></i> Privacy policy</a>
+       <a href="#"> <i class="fas fa-angle-right"></i> Terms of use</a>
+
+    </div>
+
+    <div class="box">
+        <h3>Contact info</h3>
+
+               <a href="#"> <i class="fas fa-phone"></i> +383-45-789-250 </a>
+               <a href="#"> <i class="fas fa-phone"></i> +383-44-333-363 </a>
+               <a href="#"> <i class="fas fa-envelope"></i> travelagency@gmail.com </a>
+               <a href="#"> <i class="fas fa-map"></i> Prishtine, Kosovo - 120503 </a>
+
+    </div>
+
+    <div class="box">
+        <h3>Follow us</h3>
+        <a href="#"> <i class="fab fa-facebook"> Facebook </i> </a>
+        <a href="#"> <i class="fab fa-twitter"> Twitter </i> </a>
+        <a href="#"> <i class="fab fa-instagram"> Instagram </i> </a>
+        <a href="#"> <i class="fab fa-linkedin"> Likedin </i> </a>
+
+    </div>
+
+
+</div>
+
+<div class="credit"> Created by <span>Mrs. web designer</span>  | all rights reserved!</div>
+</section>
+
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+<!-- swiper js file link -->
+ <script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
+
+
+<!-- custom js file link -->
+ <script> strc="js/main.js"</script>
+</body>
+</html>
